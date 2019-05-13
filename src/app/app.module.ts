3dc1703f8b6ad086bf56ba8bd2b13912ff38d05f
Antonio Sierra <asierra@fingeosel.com>
2019-05-13 14:48:30 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
-
+import { appRoutes } from './app.router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StarComponent } from './star/star.component';
+import { HotelListComponent } from './hotel-list/hotel-list.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StarComponent,
+    HotelListComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
